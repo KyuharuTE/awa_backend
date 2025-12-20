f6632529a3cd548929316f6df12a7afce53e9ac1
Nacho <kyuharu@qq.com>
2025-12-20 10:20:24 +0800

    feat(bot GetOnlineUser): use script api

--- scripts/bot_get_online_user.js
+++ scripts/bot_get_online_user.js
@@ -1,27 +1,18 @@
 import { logger } from "node-napcat-ts";
 import { pushUser } from "../data_base.js";
+import { defineScript } from "../script_api.js";
 
-/**
- *
- * @param {Object} ctx
- * @param {import("node-napcat-ts").NCWebsocket} ctx.napcat
- * @param {import("express").Express} ctx.app
- * @param {Map<number, import('ws').WebSocket>} ctx.wsClients
- * @param {(msg: string) => void} ctx.broadcast
- * @param {(id: number, msg: string) => void} ctx.sendToClient
- * @param {(fn: (msg: string, id: number, ws: import('ws').WebSocket) => void) => void} ctx.onWsMessage
- */
-export async function run({ napcat }) {
-	napcat.on("message.group", async (msg) => {
+export default defineScript(async (ctx) => {
+	ctx.napcat.on("message.group", async (msg) => {
 		try {
 			if (
 				msg.group_id === 819790435 &&
 				msg.raw_message === "#获取在线用户"
 			) {
-				await pushUser(napcat);
+				await pushUser(ctx.napcat);
 			}
 		} catch (error) {
 			logger.warn(error);
 		}
 	});
-}
+});
