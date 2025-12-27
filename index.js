fb2e7319544327f39a8141a8c790c749601138f5
Nacho <kyuharu@qq.com>
2025-12-27 15:07:38 +0800

    debug

--- index.js
+++ index.js
@@ -119,6 +119,7 @@ setInterval(async () => {
 	const rows = await getAllClients();
 	for (const client of rows) {
 		if (Date.now() - client.last_heartbeat > 1000 * 60 * 6) {
+			logger.debug(`Client ${client.uin} is offline.`);
 			if (client.offline == 0) {
 				await updateClient(client.uin, client.last_heartbeat, 1);
 				// util.bot_send_text(`${client.uin} 离线了喵`);
