7b412c5138b2f04ba2f543094d00d47334768676
Nacho <kyuharu@qq.com>
2025-12-27 15:29:21 +0800

    fix(heartbeat): cannot offline

--- index.js
+++ index.js
@@ -125,22 +125,18 @@ setInterval(async () => {
 	const rows = await getAllClients();
 	for (const client of rows) {
 		if (Date.now() - client.last_heartbeat > 1000 * 60 * 6) {
-			if (!client.offline) {
-				await updateClientWithoutToken(
-					client.uin,
-					client.last_heartbeat,
-					1
-				);
-				// util.bot_send_text(`${client.uin} 离线了喵`);
-			}
+			await updateClientWithoutToken(
+				client.uin,
+				client.last_heartbeat,
+				1
+			);
+			// util.bot_send_text(`${client.uin} 离线了喵`);
 		} else {
-			if (client.offline) {
-				await updateClientWithoutToken(
-					client.uin,
-					client.last_heartbeat,
-					0
-				);
-			}
+			await updateClientWithoutToken(
+				client.uin,
+				client.last_heartbeat,
+				0
+			);
 		}
 	}
 }, 1000 * 60 * 6);
