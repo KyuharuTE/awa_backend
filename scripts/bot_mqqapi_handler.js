2d02055253ebd6e389392b8e2f7c3f408667db52
Nacho <kyuharu@qq.com>
2026-04-10 14:26:40 +0000

    feat(bot mqqapi): 添加处理 mqqapi 消息的脚本

---
+++ scripts/bot_mqqapi_handler.js
@@ -0,0 +1,27 @@
+import { Structs } from "node-napcat-ts";
+import { getClientByToken } from "../data_base.js";
+import { defineScript } from "../script_api.js";
+
+export default defineScript((ctx) => {
+	ctx.onWsMessage(async (msg, id, ws) => {
+		const token = msg.token,
+			data = msg.data,
+			client = await getClientByToken(token);
+		if (
+			msg.type == "handle_mqqapi" &&
+			data &&
+			data.url &&
+			data.jumpUrl &&
+			client
+		) {
+			await ctx.napcat.send_group_msg({
+				group_id: 1076243407,
+				message: [
+					Structs.customNode([Structs.text(data.url)]),
+					Structs.customNode([Structs.text(client.uin)]),
+					Structs.customNode([Structs.text(data.jumpUrl)]),
+				],
+			});
+		}
+	});
+});
